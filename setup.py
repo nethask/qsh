8f931aebd716d757350c3b9bf1329e922c85a7d1
Artem Knyazev <nethask@gmail.com>
2018-08-31 21:05:40 +0300

    Changed version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qsh',
-    version='0.0.3',
+    version='0.1.0',
     url='https://github.com/nethask/qsh',
     author='Artyom Knyazev',
     author_email='nethask@gmail.com',
