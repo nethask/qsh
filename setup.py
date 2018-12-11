75c1431045541797757ef7ba79db561a5b5fc8a1
Artem Knyazev <nethask@gmail.com>
2018-12-11 17:05:32 +0400

    Changed version of pip package and fixed licence.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qsh',
-    version='0.5.1',
+    version='0.5.3',
     url='https://github.com/nethask/qsh',
     author='Artyom Knyazev',
     author_email='nethask@gmail.com',
