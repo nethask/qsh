c610d2390c86ec0f96f2c5981f1fc5bcd6bf870c
Artem Knyazev <nethask@gmail.com>
2019-03-10 13:23:12 +0300

    Code compilation with Cython

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+from Cython.Build import cythonize
 
 setup(
     name='qsh',
-    version='0.6.1',
+    version='0.7.0',
     url='https://github.com/nethask/qsh',
     author='Artyom Knyazev',
     author_email='nethask@gmail.com',
     license='MIT',
-    install_requires=['python-dateutil']
+    install_requires=['python-dateutil'],
+    ext_modules=cythonize('qsh/__init__.py')
 )
