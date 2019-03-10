165b568ba37b62911bc3fc78d873f84a27f8a44d
Artem Knyazev <nethask@gmail.com>
2019-03-10 14:42:48 +0300

    Fixed bug related to pip install

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
 
 setup(
     name='qsh',
-    version='0.7.3',
+    version='0.7.4',
+    packages=find_packages(),
     url='https://github.com/nethask/qsh',
     author='Artyom Knyazev',
     author_email='nethask@gmail.com',
     license='MIT',
     install_requires=['python-dateutil'],
+    ext_modules=cythonize("qsh/*.py")
 )
